436e550047006323803b3f492b8660a65db3bcee
beehash <410181756@qq.com>
2021-05-28 18:15:03 +0800

    feat: 新增模块文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,18 @@
-import React, {BaseSyntheticEvent, useState, useEffect} from 'react';
-import logo from './statics/images/logo.svg';
-import './statics/sass/App.scss';
-import HeaderApp from './components/business/Header';
+import React from 'react';
+import '@/statics/sass/App.scss';
+import Header from './components/business/Header';
+import Banner from './components/business/Banner';
 
 function App() {
   return (
     <div className="App full_gradient js-full-gradient state-full state-complete">
       {/* header */}
-      <HeaderApp/>
+      <Header/>
       {/* banner */}
-      <div className="banner">
-        <div className="view-box">
-          <div className="logo-box">
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
-        </div>
-        <div className="left-view">
-          <p>这是我的博客</p>
-          <p>Welcome to aminsey’s Blog</p>
-          <p>ここでは笹寺のブログ</p>
-        </div>
-        <div className="right-view">
-          <ul className="list">
-            <li>
-              <span className="list-item">文章</span>
-            </li>
-            <li>
-              <span className="list-item">随笔</span>
-            </li>
-            <li>
-              <span className="list-item">算法</span>
-            </li>
-            <li>
-              <span className="list-item">框架</span>
-            </li>
-            <li>
-              <span className="list-item">Javascript</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-
+      <Banner/>
       {/* Content */}
       <div className="inner-box">
-          <div className="content">
+          <div className="content theme-bg">
           </div>
         </div>
     </div>
--- src/components/business/Header.tsx
+++ src/components/business/Header.tsx
@@ -1,22 +1,52 @@
-import React, {useState, useEffect} from 'react';
+import React, {BaseSyntheticEvent, useState} from 'react';
 import logo from '@/statics/images/logo.svg';
 import '@/statics/sass/App.scss';
-import styles from '@/statics/sass/header.module.scss';
+import styles from '@/statics/sass/home.module.scss';
+import Banner from '@/components/business/Banner';
 
 export default function Header () {
   const codeAge:number = new Date().getFullYear() - 2018;
   const [theme, setTheme] = useState('#ffffff');
 
+  function handleThemeChange (event: BaseSyntheticEvent) {
+    const color: string = event.target.value;
+    setTheme(color);
+    const styleEl: HTMLStyleElement = document.createElement('style');
+    styleEl.id = 'theme';
+    let styleId = document.getElementById('theme') as HTMLStyleElement;
+    if(!styleId) {
+      document.head.appendChild(styleEl);
+      styleId = document.getElementById('theme') as HTMLStyleElement;;
+    }
+
+
+    const red = parseInt(color.slice(1, 3), 16);
+    const green = parseInt(color.slice(3, 5), 16);
+    const blue = parseInt(color.slice(5, 7), 16);
+    
+    const inverseColor = `#${(255-red).toString(16)}${(255-green).toString(16)}${(255-blue).toString(16)}`;
+
+    styleId.innerHTML = `.theme{color: ${inverseColor};}
+                        .theme-bg{background-color: ${theme}}
+                        .base-button{color: ${theme}}
+                        .primary-button{color: ${inverseColor};background-color: ${theme}}`
+  }
   return (
-    <div className="center-block header">
+    <div className="center-block header mt-8">
         <p className="left-bar">
-          <img src={logo} alt="avator" className="avator mr-8 inline-block vertical-middle"/>
-          <span className="mr-8 inline-block vertical-middle">beehash 的博客空间</span>
-          <span className="inline-block vertical-middle">码龄：{codeAge} 年</span>
+          <span className={`${styles.avator} mr-8`}><img src={logo} alt="avator"/></span>
+          <span className="mr-8 inline-block vertical-middle theme">beehash 的博客空间</span>
+          <span className="inline-block vertical-middle theme">码龄：{codeAge} 年</span>
         </p>
-        <div className={`${styles['mr-12']} right-bar`}>
-          <input type="color" value={theme} onChange={(e) => setTheme(e.target.value)}/>
+        <div className={styles['right-bar']}>
+          <span className="mr-8">
+            <label className="mr-8 theme">皮肤</label>
+            <input type="color" value={theme} onChange={handleThemeChange}/>
+          </span>
+          <button className="base-button primary-button mr-8">写文章</button>
+          <button className="base-button default-button">登录</button>
         </div>
+        <Banner/>
       </div>
   )
 }
